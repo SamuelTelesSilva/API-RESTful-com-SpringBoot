5a2309eadc49e72fb127908d2e145d9850cf4f01
SamuelTelesSilva <samuel.silva@uni9.edu.br>
2020-05-11 18:07:31 -0300

    GET - POST - DELETE - PUT

--- src/main/java/com/example/demo/api/IndexController.java
+++ src/main/java/com/example/demo/api/IndexController.java
@@ -1,6 +1,9 @@
 package com.example.demo.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
 	@GetMapping
-	public String ola() {
-		return "Hello Spring Boot";
+	public String get() {
+		return "get Spring Boot";
+	}
+	
+	@PostMapping
+	public String post() {
+		return "Post Spring Boot";
+	}
+	
+	@PutMapping
+	public String put() {
+		return "put Spring Boot";
+	}
+	
+	@DeleteMapping
+	public String delete() {
+		return "delete Spring Boot";
 	}
 	
 }
